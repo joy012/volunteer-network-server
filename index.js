45c03f5af753daa933bdc6eb53b4d2949be43d63
joy012 <joy.buet.012@gmail.com>
2020-10-10 04:34:09 +0600

    server finished

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const admin = require('firebase-admin');
+const serviceAccount = require("./config/volunteer-network-spa-firebase-adminsdk-mbnvv-1051d3a22b.json");
+require('dotenv').config()
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: `${process.env.SITE_NAME}`
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ukskk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+client.connect(err => {
+    const eventCollection = client.db("volunteerNetwork").collection("events");
+    const volunteerCollection = client.db("volunteerNetwork").collection('volunteers');
+
+    app.get('/events', (req, res) => {
+        eventCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.post('/addEvent', (req, res) => {
+        const newEvent = req.body;
+        eventCollection.insertOne(newEvent)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    app.post('/addVolunteer', (req, res) => {
+        const newVolunteer = req.body;
+        volunteerCollection.insertOne(newVolunteer)
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    app.get('/tasks', (req, res) => {
+        const bearer = req.headers.authorization;
+        if (bearer && bearer.startsWith('Bearer ')) {
+            const idToken = bearer.split(' ')[1];
+            admin.auth().verifyIdToken(idToken)
+                .then(function (decodedToken) {
+                    const tokenEmail = decodedToken.email;
+                    const queryEmail = req.query.email;
+                    if (tokenEmail === queryEmail) {
+                        volunteerCollection.find({email: queryEmail})
+                            .toArray( (err,documents) => {
+                                res.status(200).send(documents);
+                                
+                            })
+                    }
+                    else{
+                        res.status(401).send('Un-Authorized Access!!')
+                    }
+                }).catch(function (error) {
+                    res.status(401).send('Un-Authorized Access!!')
+                });
+        }
+        else{
+            res.status(401).send('Un-Authorized Access!!')
+        }
+    })
+
+})
+
+
+
+app.listen(3360);
